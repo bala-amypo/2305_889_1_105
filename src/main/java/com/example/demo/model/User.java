1bf424a4411715903b6dfebfdc27c70ff997c47a
bala-amypo <bala@amypo.in>
2025-12-18 08:38:19 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,6 +1,6 @@
 package com.example.demo.model;
 
-import
+import jakarta.persistence.*;
 public class User{
     @Id
     private Long id;
@@ -9,5 +9,54 @@ public class User{
     private String phonenumber;
     private String idProofNumber;
     private LocalDateTime createdAt;
-
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+    public String getIdProofNumber() {
+        return idProofNumber;
+    }
+    public void setIdProofNumber(String idProofNumber) {
+        this.idProofNumber = idProofNumber;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public User(Long id, String name, String email, String phonenumber, String idProofNumber, LocalDateTime createdAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phonenumber = phonenumber;
+        this.idProofNumber = idProofNumber;
+        this.createdAt = createdAt;
+    }
+    public User() {
+        
+    }
+    
+    
+  
 }
