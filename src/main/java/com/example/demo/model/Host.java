cf18eec50d68f27298fb15282b313d383b87c7a7
bala-amypo <bala@amypo.in>
2025-12-18 09:03:17 +0000

    User commit

--- src/main/java/com/example/demo/model/Host.java
+++ src/main/java/com/example/demo/model/Host.java
@@ -1,3 +1,100 @@
 package com.example.demo.model;
 
-import
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+    name = "host",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email")
+    }
+)
+public class Host {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Host name is required")
+    @Column(nullable = false)
+    private String hostName;
+
+    private String fullname;
+
+    @Email(message = "Invalid email format")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "Department is required")
+    @Column(nullable = false)
+    private String department;
+
+    @NotBlank(message = "Phone number is required")
+    @Column(nullable = false)
+    private String phone;
+
+    private LocalDateTime createdAt;
+
+  
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
