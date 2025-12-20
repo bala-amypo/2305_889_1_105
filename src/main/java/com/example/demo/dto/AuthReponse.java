de1e7bcf356be237a9fb53a4708edb7e543b6cff
bala-amypo <bala@amypo.in>
2025-12-20 12:46:07 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/AuthReponse.java
@@ -0,0 +1,54 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity 
+@Table(name = "alert_notifications")
+public class AlertNotification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "visit_log_id", nullable = false)
+    private VisitLog visitLog;
+
+    private String sentTo;
+
+    private String alertMessage;
+
+    @Column(nullable = false)
+    private LocalDateTime sentAt;
+
+    @PrePersist
+    protected void onCreate() {
+        sentAt = LocalDateTime.now();
+    }
+
+    public AlertNotification() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public VisitLog getVisitLog() { return visitLog; }
+    public void setVisitLog(VisitLog visitLog) { this.visitLog = visitLog; }
+
+    public String getSentTo() { return sentTo; }
+    public void setSentTo(String sentTo) { this.sentTo = sentTo; }
+
+    public String getAlertMessage() { return alertMessage; }
+    public void setAlertMessage(String alertMessage) { this.alertMessage = alertMessage; }
+
+    public LocalDateTime getSentAt() { return sentAt; }
+    public void setSentAt(LocalDateTime sentAt) { this.sentAt = sentAt; }
+
+    public AlertNotification(Long id, VisitLog visitLog, String sentTo, String alertMessage, LocalDateTime sentAt) {
+        this.id = id;
+        this.visitLog = visitLog;
+        this.sentTo = sentTo;
+        this.alertMessage = alertMessage;
+        this.sentAt = sentAt;
+    }
+    
+}
--- src/main/java/com/example/demo/model/AlertNotification.java
+++ src/main/java/com/example/demo/model/AlertNotification.java
@@ -3,7 +3,7 @@ package com.example.demo.model;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
-@Entity
+@Entity 
 @Table(name = "alert_notifications")
 public class AlertNotification {
     @Id
