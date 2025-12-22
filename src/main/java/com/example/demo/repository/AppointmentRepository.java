72406008407c9cee33f2f46b1c910c7f22894af7
bala-amypo <bala@amypo.in>
2025-12-22 07:54:15 +0000

    User commit

--- src/main/java/com/example/demo/repository/AppointmentRepository.java
+++ src/main/java/com/example/demo/repository/AppointmentRepository.java
@@ -1,10 +1,11 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Appointment;
+import com.example.demo.entity.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
     List<Appointment> findByHostId(Long hostId);
     List<Appointment> findByVisitorId(Long visitorId);
--- src/main/java/com/example/demo/repository/HostRepository.java
+++ src/main/java/com/example/demo/repository/HostRepository.java
@@ -1,10 +1,11 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Host;
+import com.example.demo.entity.Host;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+@Repository
 public interface HostRepository extends JpaRepository<Host, Long> {
     Optional<Host> findByEmail(String email);
 }
