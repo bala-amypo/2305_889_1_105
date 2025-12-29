e246db38ba2ae87fc463d35be4c56e89e1e9e60c
bala-amypo <bala@amypo.in>
2025-12-29 03:32:11 +0000

    User commit

--- src/main/java/com/example/demo/controller/AlertNotificationController.java
+++ src/main/java/com/example/demo/controller/AlertNotificationController.java
@@ -1,30 +1,30 @@
-// package com.example.demo.controller;
+package com.example.demo.controller;
 
-// import com.example.demo.model.AlertNotification;
-// import com.example.demo.service.impl.AlertNotificationServiceImpl;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.*;
-// import java.util.List;
+import com.example.demo.model.AlertNotification;
+import com.example.demo.service.impl.AlertNotificationServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
-// @RestController
-// @RequestMapping("/api/alerts")
-// public class AlertNotificationController {
+@RestController
+@RequestMapping("/api/alerts")
+public class AlertNotificationController {
 
-//     @Autowired
-//     private AlertNotificationServiceImpl alertService;
+    @Autowired
+    private AlertNotificationServiceImpl alertService;
 
-//     @PostMapping("/{visitLogId}")
-//     public AlertNotification sendAlert(@PathVariable Long visitLogId) {
-//         return alertService.sendAlert(visitLogId);
-//     }
+    @PostMapping("/{visitLogId}")
+    public AlertNotification sendAlert(@PathVariable Long visitLogId) {
+        return alertService.sendAlert(visitLogId);
+    }
 
-//     @GetMapping("/{id}")
-//     public AlertNotification getAlert(@PathVariable Long id) {
-//         return alertService.getAlert(id);
-//     }
+    @GetMapping("/{id}")
+    public AlertNotification getAlert(@PathVariable Long id) {
+        return alertService.getAlert(id);
+    }
 
-//     @GetMapping
-//     public List<AlertNotification> getAllAlerts() {
-//         return alertService.getAllAlerts();
-//     }
-// }
+    @GetMapping
+    public List<AlertNotification> getAllAlerts() {
+        return alertService.getAllAlerts();
+    }
+}
